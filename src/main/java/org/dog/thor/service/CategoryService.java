0345826fa69d5ed233debefc3f413b226d99ef10
zhanghaojian <1427774041@qq.com>
2022-07-14 00:10:49 +0800

    餐品功能初步实现

--- src/main/java/org/dog/thor/service/CategoryService.java
+++ src/main/java/org/dog/thor/service/CategoryService.java
@@ -9,5 +9,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2022-07-13 19:12:52
 */
 public interface CategoryService extends IService<Category> {
-
+    public void remove(Long id);
 }
